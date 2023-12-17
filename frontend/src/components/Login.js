877487ec1d3dec1f86bf431b78d6f8523cc1aba8
Sinmbf <sinmbf123@gmail.com>
2023-12-17 20:07:39 +0545

    Update

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -15,9 +15,13 @@ function Login() {
           </div>
           <div>
             <div className="icon_section">
-              <img className="icon" src="./add_circle_fill.svg" alt="" />
+              <img
+                className="icon"
+                src="https://scontent.xx.fbcdn.net/v/t1.15752-9/403417681_324462643764488_1999405666588347622_n.jpg?stp=dst-jpg_p173x172&_nc_cat=105&ccb=1-7&_nc_sid=510075&_nc_ohc=1kfQbmQ5eIMAX--2-Oq&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AdQ4z9Nd4m5eizP6g9InaFjVI0oOSnzPqhgneDU7FL-l0A&oe=65A679C3"
+                alt=""
+              />
             </div>
-            <p className="nam_e">Add Account</p>
+            <p className="nam_e">DCN</p>
           </div>
         </div>
       </div>
